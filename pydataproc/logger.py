134dffc7e403b889cbe6f5ab2df95ac0dc03e9de
Oli Hall <oliver.hall@duedil.com>
2017-10-18 18:03:50 +0100

    Add logger module

---
+++ pydataproc/logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+
+log = logging.getLogger(__name__)
+log.setLevel('INFO')
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s %(message)s',
+                                       datefmt='%Y-%m-%d %H:%M:%S'))
+log.addHandler(handler)
