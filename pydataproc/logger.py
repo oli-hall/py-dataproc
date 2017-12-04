134bb3a52cd6830208cad1602d46eb1b285408b7
Oli Hall <oliver.hall@duedil.com>
2017-12-04 18:48:42 +0000

    Remove package name from log statements
    was always log package due to logging pattern, which isn't very useful.

--- pydataproc/logger.py
+++ pydataproc/logger.py
@@ -5,6 +5,6 @@ import sys
 log = logging.getLogger(__name__)
 log.setLevel('INFO')
 handler = logging.StreamHandler(stream=sys.stderr)
-handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s %(message)s',
+handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s',
                                        datefmt='%Y-%m-%d %H:%M:%S'))
 log.addHandler(handler)
