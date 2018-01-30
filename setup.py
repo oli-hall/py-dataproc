41227aed2888c9b262feed03c06c894feaf2a4b8
Oli Hall <oliver.hall@duedil.com>
2018-01-30 10:52:31 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ test_requires = [
 
 setup(
     name='pydataproc',
-    version='0.7.0',
+    version='0.7.1',
     author='Oli Hall',
     author_email='',
     description="Python wrapper for the Google DataProc client",
